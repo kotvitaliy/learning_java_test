ee54c137d50c3ed78da431c6d11d4dd10b6cb532
Vitaliy Kotlubaev <vkotlubaev@rra-systems.com>
2020-11-25 11:25:33 +0200

    second commit

--- src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
-    private static final String RESOURCE_PATH = "src\\test\\resources\\";
+//    private static final String RESOURCE_PATH = "src\\test\\resources\\";
     private static WebDriver driver;
 
 
@@ -17,13 +17,13 @@ public class DriverSingleton {
         if (null == driver){
             switch (System.getProperty("browser")){
                 case "firefox": {
-                    System.setProperty("webdriver.gecko.driver", RESOURCE_PATH + "geckodriver.exe");
-//                    WebDriverManager.firefoxdriver().setup();
+//                    System.setProperty("webdriver.gecko.driver", RESOURCE_PATH + "geckodriver.exe");
+                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                 }
                 default: {
-                    System.setProperty("webdriver.chrome.driver", RESOURCE_PATH + "chromedriver.exe");
-                    //WebDriverManager.chromedriver().setup();
+//                    System.setProperty("webdriver.chrome.driver", RESOURCE_PATH + "chromedriver.exe");
+                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                 }
             }
